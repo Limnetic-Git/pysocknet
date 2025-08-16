cb88d8086ec07dc6997b408fda56d173d785a8e3
prepodobnuy <zvfvbycsy6@gmail.com>
2025-08-16 21:49:30 +0300

    Formatted entire file

--- pysocknet.py
+++ pysocknet.py
@@ -1,31 +1,31 @@
 import socket
 import ast
 
+
 class ClientConnection:
     def __init__(self, ip: str, port: int):
         self.ip = ip
         self.port = port
         self.ClientSocket = socket.socket()
         self.__connect()
-        
+
     def __connect(self):
         try:
             self.ClientSocket.connect((self.ip, self.port))
         except socket.error as e:
             print(str(e))
-    
+
     def receive(self, packet_size: int, raw=False):
-        received_data = self.ClientSocket.recv(packet_size).decode('utf-8')
+        received_data = self.ClientSocket.recv(packet_size).decode("utf-8")
         if raw:
             return received_data
         else:
             return ast.literal_eval(received_data)
-    
+
     def send(self, packet: str):
         self.ClientSocket.send(str.encode(packet))
-    
-    
-    
+
+
 class ServerConnection:
     def __init__(self, ip: str, port: int, max_peers_count=100):
         self.ip = ip
@@ -33,24 +33,23 @@ class ServerConnection:
         self.max_peers_count = max_peers_count
         self.ServerSocket = socket.socket()
         self.__connect()
-        
+
     def __connect(self):
         try:
             self.ServerSocket.bind((self.ip, self.port))
-            self.ServerSocket.listen(self.max_peers_count) 
+            self.ServerSocket.listen(self.max_peers_count)
         except socket.error as e:
             print(str(e))
 
     def receive(self, connection, packet_size: int, raw=False):
-        received_data = connection.recv(packet_size).decode('utf-8')
+        received_data = connection.recv(packet_size).decode("utf-8")
         if raw:
             return received_data
         else:
             return ast.literal_eval(received_data)
-    
+
     def send(self, connection, packet: str):
         connection.send(str.encode(packet))
-    
+
     def accept(self):
         return self.ServerSocket.accept()
-
