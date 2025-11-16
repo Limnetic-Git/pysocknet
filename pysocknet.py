b612051cdabf2ca492903391b808d804ef0a4353
Limnetic-Git <limneticgit@mail.ru>
2025-11-16 19:13:25 +0300

    Chunking, bytesockets

--- pysocknet.py
+++ pysocknet.py
@@ -2,6 +2,7 @@ import socket
 import ast
 from _thread import *
 import logging
+
 logging.basicConfig(level=logging.INFO, filename="log.txt", filemode="w")
 
 class TCPClientConnection:
@@ -27,27 +28,101 @@ class TCPClientConnection:
     def receive(self, packet_size: int, raw=False):
         try:
             received_data = self.ClientSocket.recv(packet_size).decode('utf-8')
-            if raw:
-                return received_data
+            if not 'CHNKS:' in received_data:
+                if raw:
+                    return received_data
+                else:
+                    return ast.literal_eval(received_data)
             else:
-                return ast.literal_eval(received_data)
+                if received_data[0] == 'C':
+                    self.ClientSocket.send(str.encode('1'))
+                    r = received_data.split(':')
+                    #print(r)
+                    CHUNKS_NEED = int(r[1])
+                    packet = ''
+                    for i in range(CHUNKS_NEED):
+                        packet = packet + self.ClientSocket.recv(3200).decode('utf-8')
+                        self.ClientSocket.send(str.encode('1'))
+                    if raw:
+                        if r[2] != 'None':
+                            return packet, r[2]
+                        else:
+                            return packet
+                    else:
+                        if r[2] != 'None':
+                            return ast.literal_eval(packet), r[2]
+                        else:
+                            return ast.literal_eval(packet)
+                        
+                elif received_data[0] == 'B':
+                    self.ClientSocket.send(str.encode('1'))
+                    r = received_data.split(':')
+                    CHUNKS_NEED = int(r[1])
+                    packet = bytes()
+                    for i in range(CHUNKS_NEED):
+                        packet = packet + self.ClientSocket.recv(3200)
+                        self.ClientSocket.send(str.encode('1'))
+                        
+                    if raw:
+                        if r[2] != 'None':
+                            return packet, r[2]
+                        else:
+                            return packet
+                    else:
+                        if r[2] != 'None':
+                            return ast.literal_eval(packet.decode('utf-8')), r[2]
+                        else:
+                            return ast.literal_eval(packet.decode('utf-8')) 
         except socket.error as e:
             logging.error(e)
             raise e
         
-    def send(self, packet: str):
-        try:
-            self.ClientSocket.send(str.encode(packet))
-        except socket.error as e:
-            logging.error(e)
-            raise e
-        
+    def send(self, packet, file_format=None):
+        if type(packet) == str:
+            FULL_LEN = len(packet)
+            CHUNK_LEN = 512
+            CHUNKS_NEED = FULL_LEN // CHUNK_LEN + (1 if FULL_LEN % CHUNK_LEN != 0 else 0)
+            if CHUNKS_NEED != 1:
+                CHUNKS = []
+                chunk = ''
+                for i in range(FULL_LEN):
+                    chunk = chunk + packet[i]
+                    if (i + 1) % CHUNK_LEN == 0:
+                        CHUNKS.append(chunk)
+                        chunk = ''
+                if chunk: CHUNKS.append(chunk)
+                
+                self.ClientSocket.send(str.encode(f'CHNKS:{CHUNKS_NEED}:{file_format}'))
+                self.ClientSocket.recv(1)
+                for i in range(CHUNKS_NEED):
+                    self.ClientSocket.send(str.encode(CHUNKS[i]))
+                    self.ClientSocket.recv(1)
+            else:
+                self.ClientSocket.send(str.encode(packet))
+                
+        elif type(packet) == bytes:
+            FULL_LEN = len(packet)
+            CHUNK_LEN = 2048
+            if FULL_LEN > CHUNK_LEN:
+                CHUNKS = []
+                chunk = bytearray()
+                for i, byte in enumerate(packet):
+                    chunk.append(byte)
+                    if (i + 1) % CHUNK_LEN == 0:
+                        CHUNKS.append(chunk)
+                        chunk = bytearray()
+                if chunk: CHUNKS.append(chunk)
+                CHUNKS_NEED = len(CHUNKS)
+                
+                self.ClientSocket.send(str.encode(f'BCHNKS:{CHUNKS_NEED}:{file_format}'))
+                self.ClientSocket.recv(1)
+                for i in range(CHUNKS_NEED):
+                    self.ClientSocket.send(CHUNKS[i])
+                    self.ClientSocket.recv(1)
+            else:
+                self.ClientSocket.send(packet)
     def close(self):
-        try:
-            self.ClientSocket.close()
-        except socket.error as e:
-            logging.error(e)
-            raise e
+        self.ClientSocket.close()
         
 class TCPServerConnection:
     def __init__(self, ip: str, port: int, max_peers_count=100):
@@ -84,20 +159,100 @@ class TCPServerConnection:
     def receive(self, connection, packet_size: int, raw=False):
         try:
             received_data = connection.recv(packet_size).decode('utf-8')
-            if raw:
-                return received_data
+            if not 'CHNKS:' in received_data:
+                if raw:
+                    return received_data
+                else:
+                    return ast.literal_eval(received_data)
             else:
-                return ast.literal_eval(received_data)
+                if received_data[0] == 'C':
+                    connection.send(str.encode('1'))
+                    r = received_data.split(':')
+                    #print(r)
+                    CHUNKS_NEED = int(r[1])
+                    packet = ''
+                    for i in range(CHUNKS_NEED):
+                        packet = packet + connection.recv(3200).decode('utf-8')
+                        connection.send(str.encode('1'))
+                    if raw:
+                        if r[2] != 'None':
+                            return packet, r[2]
+                        else:
+                            return packet
+                    else:
+                        if r[2] != 'None':
+                            return ast.literal_eval(packet), r[2]
+                        else:
+                            return ast.literal_eval(packet)
+                        
+                elif received_data[0] == 'B':
+                    connection.send(str.encode('1'))
+                    r = received_data.split(':')
+                    CHUNKS_NEED = int(r[1])
+                    packet = bytes()
+                    for i in range(CHUNKS_NEED):
+                        packet = packet + connection.recv(3200)
+                        connection.send(str.encode('1'))
+                        
+                    if raw:
+                        if r[2] != 'None':
+                            return packet, r[2]
+                        else:
+                            return packet
+                    else:
+                        if r[2] != 'None':
+                            return ast.literal_eval(packet.decode('utf-8')), r[2]
+                        else:
+                            return ast.literal_eval(packet.decode('utf-8'))
+                        
+                    
         except socket.error as e:
             logging.error(e)
-            raise e
-        
-    def send(self, connection, packet: str):
-        try:
-            connection.send(str.encode(packet))
-        except socket.error as e:
-            logging.error(e)
-            raise e
+            raise e 
+            
+    def send(self, connection, packet, file_format=None):
+        if type(packet) == str:
+            FULL_LEN = len(packet)
+            CHUNK_LEN = 512
+            CHUNKS_NEED = FULL_LEN // CHUNK_LEN + (1 if FULL_LEN % CHUNK_LEN != 0 else 0)
+            if CHUNKS_NEED != 1:
+                CHUNKS = []
+                chunk = ''
+                for i in range(FULL_LEN):
+                    chunk = chunk + packet[i]
+                    if (i + 1) % CHUNK_LEN == 0:
+                        CHUNKS.append(chunk)
+                        chunk = ''
+                if chunk: CHUNKS.append(chunk)
+                
+                connection.send(str.encode(f'CHNKS:{CHUNKS_NEED}:{file_format}'))
+                connection.recv(1)
+                for i in range(CHUNKS_NEED):
+                    connection.send(str.encode(CHUNKS[i]))
+                    connection.recv(1)
+            else:
+                connection.send(str.encode(packet))
+                
+        elif type(packet) == bytes:
+            FULL_LEN = len(packet)
+            CHUNK_LEN = 2048
+            if FULL_LEN > CHUNK_LEN:
+                CHUNKS = []
+                chunk = bytearray()
+                for i, byte in enumerate(packet):
+                    chunk.append(byte)
+                    if (i + 1) % CHUNK_LEN == 0:
+                        CHUNKS.append(chunk)
+                        chunk = bytearray()
+                if chunk: CHUNKS.append(chunk)
+                
+                connection.send(str.encode(f'BCHNKS:{CHUNKS_NEED}:{file_format}'))
+                connection.recv(1)
+                for i in range(CHUNKS_NEED):
+                    connection.send(CHUNKS[i])
+                    connection.recv(1)
+            else:
+                connection.send(packet)
         
     def accept(self):
         try:
@@ -115,6 +270,7 @@ class TCPServerConnection:
         except socket.error as e:
             logging.error(e)
             raise e    
+
     
 class UDPClientConnection:
     def __init__(self, ip: str, port: int):
@@ -123,7 +279,6 @@ class UDPClientConnection:
             self.port = port
             self.ClientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             self.ClientSocket.bind(('0.0.0.0', 0))
-            self.ClientSocket.settimeout(0.25)
             logging.info(f"UDP-client socket ({self.ip}:{self.port}) has been successfully created")
 
         except socket.error as e:
@@ -137,10 +292,7 @@ class UDPClientConnection:
                 return received_data.decode('utf-8')
             else:
                 return ast.literal_eval(received_data.decode('utf-8'))
-        except socket.timeout:
-            pass
-        
-        except Exception as e:
+        except socket.error as e:
             logging.error(e)
             raise e
         
@@ -164,6 +316,7 @@ class UDPServerConnection:
             self.ip = ip
             self.port = port
             self.ServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.ServerSocket.settimeout(5.0) 
             self.__connect()
             logging.info(f"UDP-server socket ({self.ip}:{self.port}) has been successfully created")
 
